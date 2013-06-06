c0583b49a3f008a56420124e0590522e6c7d618f
maxpaperno <max@wdg.us>
2013-06-06 13:05:52 -0400

    version tag.

--- src/configuration.h
+++ src/configuration.h
@@ -25,8 +25,8 @@ const int APPLICATIONVERSION = 102; // 1.0.1
 namespace QGCAUTOQUAD {
     const QString APP_NAME = "QGroundControl for AutoQuad";
     const QString APP_ORG = "AutoQuad";
-    const QString APP_VERSION_TXT = "1.2.1";
-    const int APP_VERSION = 121; // 1.2.0
+    const QString APP_VERSION_TXT = "1.3 BETA";
+    const int APP_VERSION = 122; // 1.2.2
 }
 
 #endif // QGC_CONFIGURATION_H
