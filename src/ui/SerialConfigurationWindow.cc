c1e6847f823ef11dfca7f21229bfbdc1475520a7
maxpaperno <max@wdg.us>
2013-06-06 07:08:48 -0400

    Move baud rates list building to shared utils.

--- src/ui/SerialConfigurationWindow.cc
+++ src/ui/SerialConfigurationWindow.cc
@@ -30,6 +30,7 @@ This file is part of the QGROUNDCONTROL project
 
 #include <QDebug>
 
+#include "MG.h"
 #include <SerialConfigurationWindow.h>
 #include <SerialLinkInterface.h>
 #include <QDir>
@@ -58,55 +59,8 @@ SerialConfigurationWindow::SerialConfigurationWindow(LinkInterface* link, QWidge
         setupPortList();
 
         // Set up baud rates
+        QList<int> supportedBaudRates = MG::SERIAL::getBaudRates();
         ui.baudRate->clear();
-		
-		// Keep track of all desired baud rates by OS. These are iterated through
-		// later and added to ui.baudRate.
-		QList<int> supportedBaudRates;
-
-		// Baud rates supported only by POSIX systems
-#if defined(Q_OS_UNIX) || defined(Q_OS_LINUX) || defined(Q_OS_DARWIN)
-		supportedBaudRates << 50;
-		supportedBaudRates << 75;
-		supportedBaudRates << 134;
-		supportedBaudRates << 150;
-		supportedBaudRates << 200;
-		supportedBaudRates << 1800;
-#endif
-
-		// Baud rates supported only by Windows
-#if defined(Q_OS_WIN)
-		supportedBaudRates << 14400;
-		supportedBaudRates << 56000;
-		supportedBaudRates << 128000;
-		supportedBaudRates << 256000;
-#endif
-
-		// Baud rates supported by everyone
-		supportedBaudRates << 110;
-		supportedBaudRates << 300;
-		supportedBaudRates << 600;
-		supportedBaudRates << 1200;
-		supportedBaudRates << 2400;
-		supportedBaudRates << 4800;
-		supportedBaudRates << 9600;
-		supportedBaudRates << 19200;
-		supportedBaudRates << 38400;
-		supportedBaudRates << 57600;
-		supportedBaudRates << 115200;
-        supportedBaudRates << 230400;
-        supportedBaudRates << 460800;
-
-#if defined(Q_OS_LINUX)
-        // Baud rates supported only by Linux
-        supportedBaudRates << 500000;
-        supportedBaudRates << 576000;
-#endif
-
-        supportedBaudRates << 921600;
-		
-		// Now actually add all of our supported baud rates to the UI.
-		qSort(supportedBaudRates.begin(), supportedBaudRates.end());
 		for (int i = 0; i < supportedBaudRates.size(); ++i) {
 			ui.baudRate->addItem(QString::number(supportedBaudRates.at(i)), supportedBaudRates.at(i));
 		}
