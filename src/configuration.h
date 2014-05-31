2838007b48d7ea1acd010545b019e7689d51db37
maxpaperno <max@wdg.us>
2014-05-31 08:55:22 -0400

    Update version string.

--- src/configuration.h
+++ src/configuration.h
@@ -23,8 +23,8 @@ const int APPLICATIONVERSION = 102; // 1.0.1
 namespace QGCAUTOQUAD {
     const QString APP_NAME = "QGroundControl for AutoQuad";
     const QString APP_ORG = "AutoQuad";
-    const QString APP_VERSION_TXT = "1.4 BETA 1";
-    const float APP_VERSION = 140.02f; // 1.4.0.02
+    const QString APP_VERSION_TXT = "1.4.0";
+    const float APP_VERSION = 140.03f; // 1.4.0.03
 }
 
 #endif // QGC_CONFIGURATION_H
